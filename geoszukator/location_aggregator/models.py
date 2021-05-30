3315bf3936e18680a86b2bf36b354388cae2208c
Maria Sagan <maja_jj@interia.pl>
2021-05-30 19:05:51 +0200

    alter database

--- geoszukator/location_aggregator/models.py
+++ geoszukator/location_aggregator/models.py
@@ -3,44 +3,56 @@ from django.db.models.deletion import CASCADE
 
 # Create your models here.
 class Continents(models.Model):
-    continent_name = models.CharField(max_length=64)
+    continent_name = models.CharField(max_length=64, unique=True)
     continent_code =  models.CharField(max_length=5)
     
     def __str__(self):
-        return f"{self.id}: {self.continent_name}"
+        return self
 
 class Languages(models.Model):
-    language = models.CharField(max_length=255)
+    language = models.CharField(max_length=255, unique=True)
     language_code = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self
 
 class Flags(models.Model):
     flag = models.CharField(max_length=64) 
     flag_emoji = models.ImageField(upload_to='flags')
-    flag_emoji_unicode = models.CharField(max_length=64)
+    flag_emoji_unicode = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self
 
 class Countries(models.Model):
-    country_name = models.CharField(max_length=255)
+    country_name = models.CharField(max_length=255, unique=True)
     country_code = models.CharField(max_length=5)
     capital = models.CharField(max_length=255)
-    country_flag_id = models.ForeignKey(Flags, on_delete=models.CASCADE, related_name="Flags")
-    continent_id = models.ForeignKey(Continents, on_delete=models.CASCADE, related_name="Continents")
-    language_id = models.ForeignKey(Languages, on_delete=models.CASCADE, related_name="Languages")
-    native_id = models.ForeignKey(Languages, on_delete=models.CASCADE, related_name="Native")
-    calling_code = models.IntegerField()
+    country_flag = models.ForeignKey(Flags, on_delete=models.CASCADE, related_name="flags")
+    continent = models.ForeignKey(Continents, on_delete=models.CASCADE, related_name="continents")
+    language = models.ForeignKey(Languages, on_delete=models.CASCADE, related_name="languages")
+    calling_code = models.CharField(max_length=10)
     is_eu = models.BooleanField()
 
-class Regions(models.Model):
-    region_name = models.CharField(max_length=255)
-    region_code = models.CharField(max_length=5)
-    country_id = models.ForeignKey(Countries, on_delete=models.CASCADE, related_name="Countries")
+    def __str__(self):
+        return self
 
+class Regions(models.Model):
+    region_name = models.CharField(max_length=255, unique=True)
+    region_code = models.CharField(max_length=5)
+    country = models.ForeignKey(Countries, on_delete=models.CASCADE, related_name="countries")
+
+    def __str__(self):
+        return self
 class Cities(models.Model):
     city_name = models.CharField(max_length=255)
-    zip = models.IntegerField()
-    latitude = models.DecimalField(max_digits=15, decimal_places=10)
-    longitude = models.DecimalField(max_digits=15, decimal_places=10)
-    region_id = models.ForeignKey(Regions, on_delete=models.CASCADE, related_name="Regions")
+    zip_value = models.CharField(max_length=24)
+    latitude = models.DecimalField(max_digits=20, decimal_places=10)
+    longitude = models.DecimalField(max_digits=20, decimal_places=10)
+    ip_code = models.CharField(max_length=24, unique=True)
+    region = models.ForeignKey(Regions, on_delete=models.CASCADE, related_name="regions")
 
-
+    def __str__(self):
+        return self
 
 
--- geoszukator/location_aggregator/urls.py
+++ geoszukator/location_aggregator/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import check_the_location
+from .views import \
+    check_the_location, \
+    delete_record_from_db, \
+    update_record_in_db
 
 urlpatterns = [
-    path('check-the-location/', check_the_location)
+    path('check-the-location/', check_the_location),
+    path('delete-record-from-db/', delete_record_from_db),
+    path('update-record-in-db/', update_record_in_db )
 ]
